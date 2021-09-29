0751466faa10006ec62e660d6aee624a69a96490
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-09-29 16:48:08 +0100

    feat: add passing code feature

--- Ruby/8kyu/high_and_low.rb
+++ Ruby/8kyu/high_and_low.rb
@@ -1,4 +1,4 @@
 def high_and_low(numbers)
   #your code here
-  "5 1"
+  numbers.split.minmax_by(&:to_i).reverse.join(' ')
 end
--- spec/high_low_spec.rb
+++ spec/high_low_spec.rb
@@ -1,7 +1,11 @@
 require '/Users/chukaokoye-ahaneku/Projects/codewars/Ruby/8kyu/high_and_low.rb'
 
 describe 'High and low' do
-  it 'returns high and low numbers of string' do
+  it 'returns high and low numbers of 5 and 1' do
     expect(high_and_low("1 2 3 4 5")).to eq("5 1")
+  end
+
+  it 'returns 5 and -3 as high and low numbers' do
+    expect(high_and_low("1 2 -3 4 5")).to eq("5 -3")
   end
 end
